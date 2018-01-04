f9b54de0fe1e33c2ff041f5640b5e165c2544154
Farkhod <sadykovfarkhod@gmail.com>
2018-01-03 23:15:13 -0600

    Python 3 Lessons Tkinter

---
+++ lesson_5.py
@@ -0,0 +1,34 @@
+# This program demonstraters a Button widgets
+# when the user clicks the Button, an info dialog box is displayed.
+
+
+import tkinter
+import tkinter.messagebox
+
+class MyGUI:
+    def __init__(self):
+        # Create the main window widget.
+        self.main_window = tkinter.Tk()
+
+        # Create a Button widget. The text "Click me!"
+        # should appear on the face pf the Button. The
+        # do_something method should be executed when
+        # the user clicks the Buttom.
+        self.my_button = tkinter.Button(self.main_window, text="Click me!", command=self.do_something)
+
+
+
+        # Pack the Button.
+        self.my_button.pack()
+
+        # Enter the tkinter main loop.
+        tkinter.mainloop()
+
+    # The do_something method is a callback function
+    # for the Button widget.
+    def do_something(self):
+        # Display an info dialog box.
+        tkinter.messagebox.showinfo('Response', 'Thanks for click the button')
+
+# Create an instance of the MyGUI class.
+my_gui = MyGUI()
